78ed016e10b3a560c453b47222e0e15a3bfaf3bb
samyblake <balkesoeren@googlemail.com>
2025-08-01 19:37:51 +0200

    feat: changed background

--- src/app/global/header/header.ts
+++ src/app/global/header/header.ts
@@ -1,19 +1,20 @@
 import { Component } from "@angular/core";
-import { NgIcon, provideIcons, provideNgIconsConfig } from "@ng-icons/core";
-import {
-  faSolidChevronDown,
-  faSolidUserSecret,
-} from "@ng-icons/font-awesome/solid";
+
+// import { NgIcon, provideIcons, provideNgIconsConfig } from "@ng-icons/core";
+// import {
+//   faSolidChevronDown,
+//   faSolidUserSecret,
+// } from "@ng-icons/font-awesome/solid";
 
 @Component({
   selector: "app-header",
-  imports: [NgIcon],
-  providers: [
-    provideIcons({ faSolidUserSecret, faSolidChevronDown }),
-    provideNgIconsConfig({
-      color: "#000",
-    }),
-  ],
+  // imports: [NgIcon],
+  // providers: [
+  //   provideIcons({ faSolidUserSecret, faSolidChevronDown }),
+  //   provideNgIconsConfig({
+  //     color: "#000",
+  //   }),
+  // ],
   templateUrl: "./header.html",
   styleUrl: "./header.scss",
 })
